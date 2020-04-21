b4c97ece815e932a7eb851aca083fca093401f56
Nestor Martinez <darkalex595@gmail.com>
2020-04-21 00:13:50 -0500

    Esqueleto de la app

---
+++ appMayusculas-IOSProject-master/appMayusculas/ViewControllerRespuestas2.swift
@@ -0,0 +1,34 @@
+//
+//  ViewControllerRespuestas2.swift
+//  appMayusculas
+//
+//  Created by user164056 on 4/20/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerRespuestas2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Volver(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
---
+++ appMayusculas-IOSProject-master/appMayusculas/ViewControllerResultados4.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerResultados4.swift
+//  appMayusculas
+//
+//  Created by user164056 on 4/21/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerResultados4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
